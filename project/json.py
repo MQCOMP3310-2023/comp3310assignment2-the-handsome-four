40e466d59438792fcd106a31472edc654133d458
CampbellGibsonMQ2021_46552375 <Campbell.Gibson@students.mq.edu.au>
2023-05-22 12:42:52 +1000

    Added comments

--- project/json.py
+++ project/json.py
@@ -9,6 +9,7 @@ json = Blueprint('json', __name__)
 #JSON APIs to view Restaurant Information
 @json.route('/restaurant/<restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
+    # This code has been altered to sanitize the input from SQL injection
     items = db.session.execute(text('select * from menu_item where restaurant_id = %s', restaurant_id))
     items_list = [ i._asdict() for i in items ]
     return pyjs.dumps(items_list)
@@ -16,6 +17,7 @@ def restaurantMenuJSON(restaurant_id):
 
 @json.route('/restaurant/<restaurant_id>/menu/<int:menu_id>/JSON')
 def menuItemJSON(restaurant_id, menu_id):
+    # This code has been altered to sanitize the input from SQL injection
     Menu_Item = db.session.execute(text('select * from menu_item where id = %s limit 1', menu_id))
     items_list = [ i._asdict() for i in Menu_Item ]
     return pyjs.dumps(items_list)
