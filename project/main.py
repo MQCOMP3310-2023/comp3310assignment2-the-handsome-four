07942196f908af2df3df65851174b7222fb2f839
CampbellGibsonMQ2021_46552375 <Campbell.Gibson@students.mq.edu.au>
2023-05-26 21:18:03 +1000

    updated redirect

--- project/main.py
+++ project/main.py
@@ -128,4 +128,4 @@ def login_post():
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user)
-    return redirect(url_for('main.login'))
+    return redirect(url_for('main.showRestaurants'))
