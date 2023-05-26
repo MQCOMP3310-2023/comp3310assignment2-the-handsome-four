2bb69f21aff96e1cb13eeee219de58e2f02eac77
Hasham <87964616+Hashy01@users.noreply.github.com>
2023-05-26 20:15:00 +1000

    flask_login libraries

--- project/main.py
+++ project/main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_required, current_user
 from .models import Restaurant, MenuItem
 from sqlalchemy import asc
 from . import db
