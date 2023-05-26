2d335581c8dc5171066ed97ba3e201adfb1f60c3
CampbellGibsonMQ2021_46552375 <Campbell.Gibson@students.mq.edu.au>
2023-05-26 21:28:00 +1000

    import current app

--- project/main.py
+++ project/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, current_app
 from flask_login import login_required, current_user, login_user, logout_user
 from .models import Restaurant, MenuItem, User
 from sqlalchemy import asc
@@ -117,6 +117,7 @@ def login_post():
 
     email = request.form.get('email')
     password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
     user = User.query.filter_by(email=email).first()
 
     # check if the user actually exists
@@ -127,5 +128,11 @@ def login_post():
         return redirect(url_for('main.login')) # if the user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
-    login_user(user)
+    login_user(user, remember=remember)
+    return redirect(url_for('main.showRestaurants'))
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user();
     return redirect(url_for('main.showRestaurants'))
